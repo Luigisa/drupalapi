57ae60db40ea1de6d09be61f44cb4cf975c4ba31
Luis Gil <luis.gil@biko2.com>
2019-02-26 15:40:01 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# drupalapi
---
+++ src/Hello.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace HelloWorld;
+
+/**
+ * Class SayHello.
+ *
+ * @package HelloWorld
+ */
+class SayHello {
+
+  /**
+   * Hello.
+   */
+  public static function world() {
+    return 'Hello World, Composer!';
+  }
+}
