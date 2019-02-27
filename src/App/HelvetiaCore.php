cab9c353fd0d8c6656d48e83874c715593ac9323
Luis Gil <luis.gil@biko2.com>
2019-02-27 12:23:44 +0100

    Pruebas api

--- src/App/HelvetiaCore.php
+++ src/App/HelvetiaCore.php
@@ -2,21 +2,27 @@
 
 namespace Helvetia\CoreLib\App;
 
+use GuzzleHttp\Client;
+
 class HelvetiaCore
 {
-  /**
-   * @var string
-   */
+
   private $serverUri;
+
+  private $action;
+
+  private $user;
 
   /**
    * HelvetiaCore constructor.
    *
    * @param string $serverUri
    */
-  public function __construct(string $serverUri)
+  public function __construct(string $serverUri, string $action, string $user)
   {
     $this->serverUri = $serverUri;
+    $this->action = $action;
+    $this->user = $user;
   }
 
   /**
@@ -24,8 +30,32 @@ class HelvetiaCore
    *
    * @return HelvetiaCore
    */
-  public static function build(string $serverUri): HelvetiaCore
+  public static function build(string $serverUri, string $action, string $user): HelvetiaCore
   {
-    return new self($serverUri);
+    return new self($serverUri, $action, $user);
+  }
+
+  public function execute() {
+
+    if($this->action == 'getDatosUsuario') {
+      $response = $this->getDatosUsuario();
+    }
+
+    return $response;
+  }
+
+  private function getDatosUsuario() {
+    $baseUri = 'http://wsacpt.helvetia.es';
+    $requestUri = '/rest/b2b/v1/users/' . $this->user;
+
+    $client = new Client([
+      'base_uri' => $baseUri,
+    ]);
+
+    $response = $client->request('GET', $requestUri, [
+      'query' => ['codUsuario' => 'MARISANT']
+    ]);
+
+    return $response->getBody()->getContents();
   }
 }
