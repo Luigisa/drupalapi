d5e8bf0a7f0676f52ee5817e884237f79d609e3a
Luis Gil <luis.gil@biko2.com>
2019-02-27 10:23:14 +0100

    Añadido docs y excluidos ficheros de idea

--- src/App/HelvetiaCore.php
+++ src/App/HelvetiaCore.php
@@ -4,30 +4,28 @@ namespace Helvetia\CoreLib\App;
 
 class HelvetiaCore
 {
+  /**
+   * @var string
+   */
+  private $serverUri;
 
-    /**
-     * @var string
-     */
-    private $serverUri;
+  /**
+   * HelvetiaCore constructor.
+   *
+   * @param string $serverUri
+   */
+  public function __construct(string $serverUri)
+  {
+    $this->serverUri = $serverUri;
+  }
 
-    /**
-     * HelvetiaCore constructor.
-     *
-     * @param string $serverUri
-     */
-    public function __construct(string $serverUri)
-    {
-        $this->serverUri = $serverUri;
-    }
-
-    /**
-     * @param string $serverUri
-     *
-     * @return HelvetiaCore
-     */
-    public static function build(string $serverUri): HelvetiaCore
-    {
-        return new self($serverUri);
-    }
-
+  /**
+   * @param string $serverUri
+   *
+   * @return HelvetiaCore
+   */
+  public static function build(string $serverUri): HelvetiaCore
+  {
+    return new self($serverUri);
+  }
 }
